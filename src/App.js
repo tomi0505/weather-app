714b6a57d0098edcf72e5db615205a85c1c6ef04
tomi0505 <tgdevit@gmail.com>
2026-09-24 18:10:38 +0200

    created box component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,36 @@ function formattedDate(dateStr) {
   return formatted;
 }
 
+function Box({
+  todayTitle,
+  date,
+  temp,
+  conditionIcon,
+  conditionText,
+  chanceOfRain,
+  chanceOfSnow,
+  cloud,
+}) {
+  return (
+    <div className="weather-app-b__today">
+      <div className="weather-app-b__date">
+        <h2 className="weather-app-b__today-title">{todayTitle}</h2>
+        <span>{date}</span>
+      </div>
+      <h2 className="weather-app-b__temp">{temp}&deg;C</h2>
+      <h3 className="weather-app-b__condition">
+        <img src={conditionIcon} alt={conditionText} />
+        {conditionText}
+      </h3>
+      <div className="weather-app-b__chances">
+        <div>Szanse na deszcz: {chanceOfRain}%</div>
+        <div>Szanse na śnieg: {chanceOfSnow}%</div>
+        <div>Pochmurno: {cloud}%</div>
+      </div>
+    </div>
+  );
+}
+
 function FetchWeather({ latitude, longitude }) {
   const [appData, setAppData] = useState(null);
 
@@ -29,21 +59,17 @@ function FetchWeather({ latitude, longitude }) {
   return (
     <div className="weather-app-b">
       <h1 className="weather-app-b__city">{appData.location.name}</h1>
-      <div className="weather-app-b__today">
-        <div className="weather-app-b__date">
-          <h2 className="weather-app-b__today-title">Dziś</h2>
-          <span>{formattedDate(appData.current.last_updated)}</span>
-        </div>
-        <h2 className="weather-app-b__temp">{appData.current.temp_c}&deg;C</h2>
-        <h3 className="weather-app-b__condition">
-          <img
-            src={appData.current.condition.icon}
-            alt={appData.current.condition.text}
-          />
-          {appData.current.condition.text}
-          {console.log(appData)}
-        </h3>
-      </div>
+      <Box
+        todayTitle="Dziś"
+        date={formattedDate(appData.current.last_updated)}
+        temp={appData.current.temp_c}
+        conditionIcon={appData.current.condition.icon}
+        conditionText={appData.current.condition.text}
+        chanceOfRain={appData.current.chance_of_rain}
+        chanceOfSnow={appData.current.chance_of_snow}
+        cloud={appData.current.cloud}
+      />
+      {console.log(appData)}
     </div>
   );
 }
