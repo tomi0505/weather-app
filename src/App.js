0c0176cd0b6428bce39324f3a031c8c11afd798a
tomi0505 <tgdevit@gmail.com>
2026-09-25 07:55:05 +0200

    rwd ok

--- src/App.js
+++ src/App.js
@@ -16,23 +16,27 @@ function Box({
   conditionText,
   chanceOfRain,
   chanceOfSnow,
-  cloud,
 }) {
   return (
-    <div className="weather-app-b__today">
-      <div className="weather-app-b__date">
-        <h2 className="weather-app-b__today-title">{todayTitle}</h2>
+    <div
+      className={
+        todayTitle === "Dziś"
+          ? "weather-app-box-b"
+          : "weather-app-box-b weather-app-box-b--tommorow"
+      }
+    >
+      <div className="weather-app-box-b__date">
+        <h2 className="weather-app-box-b__today-title">{todayTitle}</h2>
         <span>{date}</span>
       </div>
-      <h2 className="weather-app-b__temp">{temp}&deg;C</h2>
-      <h3 className="weather-app-b__condition">
+      <h2 className="weather-app-box-b__temp">{temp}&deg;C</h2>
+      <h3 className="weather-app-box-b__condition">
         <img src={conditionIcon} alt={conditionText} />
         {conditionText}
       </h3>
-      <div className="weather-app-b__chances">
+      <div className="weather-app-box-b__chances">
         <div>Szanse na deszcz: {chanceOfRain}%</div>
         <div>Szanse na śnieg: {chanceOfSnow}%</div>
-        <div>Pochmurno: {cloud}%</div>
       </div>
     </div>
   );
@@ -53,22 +57,36 @@ function FetchWeather({ latitude, longitude }) {
   }, [latitude, longitude]);
 
   if (!appData) {
-    return <div className="container">Ładowanie...</div>;
+    return <div>Ładowanie...</div>;
   }
 
   return (
     <div className="weather-app-b">
       <h1 className="weather-app-b__city">{appData.location.name}</h1>
-      <Box
-        todayTitle="Dziś"
-        date={formattedDate(appData.current.last_updated)}
-        temp={appData.current.temp_c}
-        conditionIcon={appData.current.condition.icon}
-        conditionText={appData.current.condition.text}
-        chanceOfRain={appData.current.chance_of_rain}
-        chanceOfSnow={appData.current.chance_of_snow}
-        cloud={appData.current.cloud}
-      />
+      <div className="weather-app-boxes-container-b">
+        <Box
+          todayTitle="Dziś"
+          date={formattedDate(appData.current.last_updated)}
+          temp={appData.current.temp_c}
+          conditionIcon={appData.current.condition.icon}
+          conditionText={appData.current.condition.text}
+          chanceOfRain={appData.current.chance_of_rain}
+          chanceOfSnow={appData.current.chance_of_snow}
+        />
+        <Box
+          todayTitle="Jutro"
+          date={formattedDate(appData.forecast.forecastday[1].date)}
+          temp={appData.forecast.forecastday[1].day.avgtemp_c}
+          conditionIcon={appData.forecast.forecastday[1].day.condition.icon}
+          conditionText={appData.forecast.forecastday[1].day.condition.text}
+          chanceOfRain={
+            appData.forecast.forecastday[1].day.daily_chance_of_rain
+          }
+          chanceOfSnow={
+            appData.forecast.forecastday[1].day.daily_chance_of_snow
+          }
+        />
+      </div>
       {console.log(appData)}
     </div>
   );
@@ -92,11 +110,11 @@ function App() {
   }, []);
 
   if (!coords) {
-    return <div className="container">Pobieranie lokalizacji...</div>;
+    return <div>Pobieranie lokalizacji...</div>;
   }
 
   return (
-    <div className="container">
+    <div>
       <FetchWeather latitude={coords.latitude} longitude={coords.longitude} />
     </div>
   );
